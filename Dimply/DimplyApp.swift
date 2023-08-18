a486232dd7fe4cd2deb56bcd1681a4c634b27c54
R <rajifafif@gmail.com>
2023-08-18 09:52:51 +0700

    Initial Commit

---
+++ Dimply/DimplyApp.swift
@@ -0,0 +1,20 @@
+//
+//  DimplyApp.swift
+//  Dimply
+//
+//  Created by Rajif Afif on 18/08/23.
+//
+
+import SwiftUI
+
+@main
+struct DimplyApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
